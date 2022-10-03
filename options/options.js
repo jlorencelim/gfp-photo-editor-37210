a76b3840332e2677c9962578895f3ff119ef822e
Crowdbotics <team@crowdbotics.com>
2022-10-03 20:40:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gfp_photo_editor_37210",
+  url: "https://gfp_photo_editor_37210.botics.co",
+  api: "https://gfp_photo_editor_37210.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
