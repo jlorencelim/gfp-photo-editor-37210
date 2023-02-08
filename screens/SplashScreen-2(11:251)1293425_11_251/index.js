2c97f710a2c6f4120c2b521ae2325793a8a8e1a5
Crowdbotics <engineering@crowdbotics.com>
2023-02-08 21:06:41 +0000

    Update components

---
+++ screens/SplashScreen-2(11:251)1293425_11_251/index.js
@@ -0,0 +1,135 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_11_255}>
+        <View style={styles.View_I11_255_379_5469}>
+          <Text style={styles.Text_I11_255_379_5469}>Get Started</Text>
+        </View>
+      </View>
+      <View style={styles.View_11_259}>
+        <View style={styles.View_11_254}>
+          <Text style={styles.Text_11_254}>Edit Images Like a Pro</Text>
+        </View>
+        <ImageBackground style={styles.ImageBackground_11_250} />
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("111%") },
+  View_11_255: {
+    width: wp("84%"),
+    minWidth: wp("84%"),
+    height: hp("8%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("8%"),
+    top: hp("93%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I11_255_379_5469: {
+    flexGrow: 1,
+    width: wp("25%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("29%"),
+    top: hp("2%"),
+    justifyContent: "flex-start"
+  },
+  Text_I11_255_379_5469: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 13,
+    fontWeight: "700",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_11_259: {
+    width: wp("51%"),
+    minWidth: wp("51%"),
+    height: hp("22%"),
+    minHeight: hp("22%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("24%"),
+    top: hp("44%")
+  },
+  View_11_254: {
+    width: wp("51%"),
+    minWidth: wp("51%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("19%"),
+    justifyContent: "flex-start"
+  },
+  Text_11_254: {
+    color: "rgba(16, 73, 9, 1)",
+    fontSize: 14,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  ImageBackground_11_250: {
+    width: wp("34%"),
+    minWidth: wp("34%"),
+    height: hp("17%"),
+    minHeight: hp("17%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("9%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 1)",
+    opacity: 0.20000000298023224
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
