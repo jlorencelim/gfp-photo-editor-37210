82d741f87a98d61fb84a51cce59415e74cca5418
Crowdbotics <team@crowdbotics.com>
2023-02-09 05:51:19 +0000

    Add Copy of Splash Screen - 1 (11:242) 1

---
+++ screens/CopyofSplashScreen-1(11:242)1/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const CopyofSplashScreen-1(11:242)1 = () => {
+  return <View style={_styles.CopyofSplashScreen-1(11:242)1}>
+      </View>;
+};
+
+export default CopyofSplashScreen-1(11:242)1;
+
+const _styles = StyleSheet.create({
+  CopyofSplashScreen-1(11:242)1: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
