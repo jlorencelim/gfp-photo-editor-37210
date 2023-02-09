214d10e0924edb14503bb82c7b40693552e5e61c
Crowdbotics <engineering@crowdbotics.com>
2023-02-09 05:50:39 +0000

    Update components

---
+++ screens/SplashScreen-1(11:242)1293429_11_242/index.js
@@ -0,0 +1,104 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_11_243}>
+        <ImageBackground
+          source={{
+            uri:
+              "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/21a12207-11cb-4fcc-84c6-1b1a68d26aee"
+          }}
+          style={styles.ImageBackground_11_244}
+        />
+        <View style={styles.View_11_245}>
+          <Text style={styles.Text_11_245}>Edit Images Like a Pro</Text>
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  View_2: { height: hp("111%") },
+  View_11_243: {
+    width: wp("51%"),
+    minWidth: wp("51%"),
+    height: hp("22%"),
+    minHeight: hp("22%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("24%"),
+    top: hp("44%")
+  },
+  ImageBackground_11_244: {
+    width: wp("34%"),
+    minWidth: wp("34%"),
+    height: hp("17%"),
+    minHeight: hp("17%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("9%"),
+    top: hp("0%"),
+    resizeMode: "cover"
+  },
+  View_11_245: {
+    width: wp("51%"),
+    minWidth: wp("51%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("19%"),
+    justifyContent: "flex-start"
+  },
+  Text_11_245: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 14,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
